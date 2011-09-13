be9c986984595b310d4e720c9d0f0f33dae5bcbd
Rick Osborne <rosborne@fullsail.com>
2011-09-13 13:26:30 -0400

    viddler.js now allows bare api calls

--- html-template/viddler.js
+++ html-template/viddler.js
@@ -5,8 +5,8 @@
    	var lastRequest = 0;
    	var viddler;
    	var api = function(callName, callMethod, callArguments, callback) {
-   		// cl("[jsViddler:api:]");
    		var requestId = "" + (++lastRequest);
+   		cl("[jsViddler:api:" + requestId + "]");
    		openRequests[requestId] = callback;
    		viddler.jsonApiRequest(callName, callMethod, callArguments, requestId);
    	};
@@ -17,6 +17,7 @@
 		}
 		var callback = openRequests[requestId];
 		delete openRequests[requestId];
+		cl("[jsViddler:callback]", data, callback);
 		if (callback) {
 			var r = JSON.parse(data);
 			// cl("[jsViddler:callback]", r);
@@ -37,6 +38,7 @@
 		setCallback: function(cb) { window.onViddler = cb; },
 		openRequests: function() { return openRequests; },
 		setKey: function(key) { viddler.setKey(key); },
+		apiCall: function(n,m,a,cb) { api(n, m, a, cb); },
 		echo: function(m, cb) { api("viddler.api.echo", "GET", { message: m }, function(r) { cb(r.echo_response.message); }); },
 		getUserVideos: function(u, cb) { api("viddler.videos.getByUser", "GET", { user: u }, function(r) { cb(r.list_result); }); } 
 	};
